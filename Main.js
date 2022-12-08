01e7ff86622448c3ffb387654b7bbcc32ea793d7
EmersonGbarros <118782060+EmersonGbarros@users.noreply.github.com>
2022-12-08 00:54:58 -0300

    ok

---
+++ Main.js
@@ -0,0 +1,21 @@
+const forms = document.getElementById('cadastro');
+
+function comparar(x,y){
+  let resultado = y > x;
+  return resultado;
+}
+
+forms.addEventListener('submit',function(evento){
+  evento.preventDefault();
+  const idade = document.getElementById('idade-usuario');
+  const idade_mae = document.getElementById('idade-mae');
+  const validaçao = comparar(idade.value,idade_mae.value);
+  const sucesso = document.querySelector('.ok')
+  if(validaçao){
+    sucesso.innerHTML = "Tá tudo Funcionando Maluco";
+    sucesso.style.display = 'block';
+    sucesso.style.co= "#AA0000";
+  }else{
+    sucesso.innerHTML = "Tem algo errado nessa Parada";
+    sucesso.style.display = 'block';
+})
